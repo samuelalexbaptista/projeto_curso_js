3a824c9edbecced80cc7af67d08d53d3b6ce51f8
Samuel <samuel.abs93@gmail.com>
2024-02-13 14:04:10 -0300

    Primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+// função que adiciona tarefa
+
+function addTask() {
+
+    // título da tarefa
+    const taskTitle = document.querySelector('#task-title').value;
+
+    if(taskTitle) {
+
+        // clona template
+        const template = document.querySelector('.template');
+
+        const newTask = template.cloneNode(true);
+
+        // adiciona titulo
+        newTask.querySelector('.task-title').textContent = taskTitle;
+
+        //remover as classes desnessárias
+        newTask.classList.remove('template');
+        newTask.classList.remove('hide');
+
+        // adiciona tarefa na lista
+        const list = document.querySelector('#task-list');
+
+        list.appendChild(newTask);
+
+        // remover tarefas
+        const removeBtn = newTask.querySelector('.remove-btn').addEventListener('click', function() {
+            removeTask(this);
+        });
+
+        // adicionar evento de completar tarefa
+        const doneBtn = newTask.querySelector('.done-btn').addEventListener('click', function() {
+            completeTask(this);
+        });
+
+        //limpar texto
+        document.querySelector('#task-title').value = '';
+
+    };
+
+}
+
+// função de completar tarefa
+function completeTask(task) {
+    const taskToComplete = task.parentNode;
+
+    taskToComplete.classList.toggle('done');
+}
+
+// função de remover tarefa
+function removeTask(task) {
+    task.parentNode.remove(true);
+}
+
+// evento de adicionar tarefa
+
+const addBtn = document.querySelector('#add-btn');
+
+addBtn.addEventListener('click', function(e) {
+
+    e.preventDefault();
+
+    addTask();
+
+})
